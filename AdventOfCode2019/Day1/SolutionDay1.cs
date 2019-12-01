d44e97b102d51b8169f8fa5fe9caf5253cdcbc0c
Patryk Urban <1850832+urbanek32@users.noreply.github.com>
2019-12-01 23:40:21 +0100

    Day1 all

---
+++ AdventOfCode2019/Day1/SolutionDay1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2019.Day1
+{
+    public class SolutionDay1
+    {
+        public void RunSolution1()
+        {
+            var input = File.ReadAllLines("Day1/input.txt").Select(int.Parse);
+            var total = input.Sum(mass => mass / 3 - 2);
+            Console.WriteLine(total);
+        }
+
+        public void RunSolution2()
+        {
+            var input = File.ReadAllLines("Day1/input.txt").Select(int.Parse);
+            //var input = new[] { 100756 };
+            var total = 0;
+            foreach (var mass in input)
+            {
+                var currentMass = mass;
+                do
+                {
+                    var fuel = currentMass / 3 - 2;
+                    if (fuel <= 0)
+                    {
+                        break;
+                    }
+                    total += fuel;
+                    currentMass = fuel;
+                }
+                while (true);
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ AdventOfCode2019/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AdventOfCode2019.Day1;
+
+namespace AdventOfCode2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new SolutionDay1().RunSolution2();
+            Console.ReadKey(false);
+        }
+    }
+}
