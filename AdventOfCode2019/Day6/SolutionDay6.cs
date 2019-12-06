803543aea7471cd0076d00d141145f114061e703
Patryk Urban <1850832+urbanek32@users.noreply.github.com>
2019-12-06 22:11:35 +0100

    Day6 all

---
+++ AdventOfCode2019/Day6/SolutionDay6.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2019.Day6
+{
+    public class SolutionDay6
+    {
+        private static Dictionary<string, string> _orbits;
+        public void RunSolution1()
+        {
+            _orbits = File.ReadAllLines("Day6/input.txt")
+                .Select(l => l.Split(")"))
+                .ToDictionary(s => s[1], s => s[0]);
+
+            var sum = _orbits
+                .Select(o => o.Value)
+                .Sum(GetDepth);
+
+            Console.WriteLine($"Answer: {sum}");
+        }
+
+        private static int GetDepth(string parent)
+        {
+            if (_orbits.TryGetValue(parent, out var orbiter))
+            {
+                return GetDepth(orbiter) + 1;
+            }
+
+            return 1;
+        }
+
+        public void RunSolution2()
+        {
+            _orbits = File.ReadAllLines("Day6/input.txt")
+                .Select(l => l.Split(")"))
+                .ToDictionary(s => s[1], s => s[0]);
+
+            var youObject = "YOU";
+            var santaObject = "SAN";
+            var youChain = new List<string>();
+            var santaChain = new List<string>();
+
+            while (youObject != "COM")
+            {
+                youChain.Add(_orbits[youObject]);
+                youObject = _orbits[youObject];
+            }
+
+            while (santaObject != "COM")
+            {
+                santaChain.Add(_orbits[santaObject]);
+                santaObject = _orbits[santaObject];
+            }
+
+            var commonObjects = youChain
+                .Except(santaChain)
+                .Union(santaChain.Except(youChain));
+
+            Console.WriteLine($"Answer: {commonObjects.Count()}");
+        }
+    }
+}
--- AdventOfCode2019/Program.cs
+++ AdventOfCode2019/Program.cs
@@ -4,6 +4,7 @@ using AdventOfCode2019.Day2;
 using AdventOfCode2019.Day3;
 using AdventOfCode2019.Day4;
 using AdventOfCode2019.Day5;
+using AdventOfCode2019.Day6;
 
 namespace AdventOfCode2019
 {
@@ -11,7 +12,7 @@ namespace AdventOfCode2019
     {
         static void Main(string[] args)
         {
-            new SolutionDay5().RunSolution2();
+            new SolutionDay6().RunSolution2();
             Console.ReadKey(false);
         }
     }
